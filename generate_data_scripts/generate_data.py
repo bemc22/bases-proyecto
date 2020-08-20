636a2e0e7da78fcd1f1778366f3482e87bd39ac2
Brayan <brayansneider22@gmail.com>
2020-08-20 17:15:25 -0500

    add generate roles

--- generate_data_scripts/generate_data.py
+++ generate_data_scripts/generate_data.py
@@ -1,4 +1,5 @@
 import csv
+from random import *
 
 
 def generar_persona():
@@ -29,3 +30,55 @@ def generar_carrera():
         row =  '(' + str(id) + ',"' + i + '"),'
         id +=1
         print(row)
+
+import random
+import string
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+def generar_roles():
+    estudiantes = []
+    for i in range(1,5101):
+        estudiantes.append(i)
+    profesor = sample(ids,100)
+    for i in profesor:
+        estudiantes.remove(i)
+
+    id = 1
+
+    print("Tabla profesores")
+    for j in profesor:
+        rng = random.random()
+        if rng >= 0.6:
+            depen = 'P'
+        else:
+            depen = 'C'
+
+
+        row = '(' + str(id) + ',' + str(j) + ",'" + depen + "'," + '"' + get_random_string(8) + '"),'
+        print(row)
+        id += 1
+
+    id = 1
+    for j in estudiantes:
+        rng = random.random()
+        if rng <= 0.1:
+            carrera = 4
+        elif rng <= 0.2:
+            carrera = 3
+        elif rng <= 0.3:
+            carrera = 2
+        else:
+            carrera = 1
+        semestre = random.randint(6, 10)
+
+        row = '(' + str(id) + ',' + str(j) + ',' + str(carrera) + ',' + str(semestre) + '),'
+        print(row)
+        id += 1
+
+
+generar_roles()
