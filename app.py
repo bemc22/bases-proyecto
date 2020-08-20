cea40b6e4cb7d85c33491f1675b2fc6e53a437af
Brayan <brayansneider22@gmail.com>
2020-08-20 16:20:55 -0500

    mas cambios

--- app.py
+++ app.py
@@ -13,15 +13,20 @@ app.config['MYSQL_DATABASE_DB'] = 'galea'
 mysql = MySQL()
 mysql.init_app(app)
 
+
+@app.route('/')
+def inicio():
+    return render_template('header.html.jinja')
+
 @app.route('/<nombre_tabla>')
-def inicio(nombre_tabla):
+def tabla(nombre_tabla):
 
     cursor = mysql.get_db().cursor()
     data, nombres = consultar(nombre_tabla,cursor)
     print(nombres)
     print(nombre_tabla)
 
-    return render_template('index.html.jinja' , data=data, nombres=nombres, nombre_tabla=nombre_tabla)
+    return render_template('crud.html.jinja' , data=data, nombres=nombres, nombre_tabla=nombre_tabla)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
