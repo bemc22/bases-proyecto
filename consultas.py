0b391df8dc3d51810bf9c77dd0f27f7055b82cc9
Brayan <brayansneider22@gmail.com>
2020-08-21 21:42:06 -0500

    Prueba 2

--- consultas.py
+++ consultas.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import  text
 from app import db
 
-# Comentario
+# Comentario , Comentario editado
 
 def consultar(nombre_tabla):
     consulta = text("SELECT * FROM " + nombre_tabla)
