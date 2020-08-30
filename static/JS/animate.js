d82d35c561aa0850180215e298570c695d3f1148
Brayan <brayansneider22@gmail.com>
2020-08-30 13:56:17 -0500

    segundo intento

--- static/JS/animate.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-
-    $('#sidebarCollapse').off('click').on('click', function () {
-      console.log('Funciona');
-        $('#sidebar').toggleClass('active');
-    });
-
-});
