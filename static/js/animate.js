9557a115bc7ddc50977503b05dd36efb3f8163e5
ivan <idop726@gmail.com>
2020-09-03 01:42:00 -0500

    A few frontend changes

--- static/js/animate.js
+++ static/js/animate.js
@@ -1,8 +1,27 @@
+// Configurando para poder ocultar el slidebar cuando se ingresa en alguna la sesion
 $(document).ready(function () {
-
     $('#sidebarCollapse').off('click').on('click', function () {
-      console.log('Funciona');
-        $('#sidebar').toggleClass('active');
+      $('#sidebar').toggleClass('active');
     });
 
+    $('.b-us').on('click', function () {
+      $('#opcion-usuario').addClass('show');
+      $('#opcion-estudiante').removeClass('show');
+    });
+
+    $('.b-es').on('click', function () {
+      $('#opcion-usuario').removeClass('show');
+      $('#opcion-estudiante').addClass('show');
+    });
 });
+
+
+// Configurando alertas:
+function notifica_error(msg){
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: msg,
+    footer: 'Si no recuerda su contraseña, por favor consulte al admin encargado'
+  })
+}
