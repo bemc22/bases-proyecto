c2aa4bc112dc5a600a8c5bdfeefb9e23cd9b2777
ivan <idop726@gmail.com>
2020-09-08 10:36:32 -0500

    little change.

--- static/js/test_graph.js
+++
@@ -1,8 +0,0 @@
-      // Load the Visualization API and the corechart package.
-      google.charts.load('current', {'packages':['bar']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
